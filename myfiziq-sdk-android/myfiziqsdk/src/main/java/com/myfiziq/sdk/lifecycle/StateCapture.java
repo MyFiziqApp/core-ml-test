7a4a9595cbe2ba95fe48e5ec7e1b433ad860d18b
Victoria <victoria@myfiziq.com>
2020-09-29 15:21:03 +0800

    remove references to deleted file from statecapture

--- myfiziq-sdk-android/myfiziqsdk/src/main/java/com/myfiziq/sdk/lifecycle/StateCapture.java
+++ myfiziq-sdk-android/myfiziqsdk/src/main/java/com/myfiziq/sdk/lifecycle/StateCapture.java
@@ -6,8 +6,6 @@ import com.myfiziq.sdk.db.Centimeters;
 import com.myfiziq.sdk.db.Gender;
 import com.myfiziq.sdk.db.Kilograms;
 import com.myfiziq.sdk.db.ModelSetting;
-import com.myfiziq.sdk.enums.ParameterSetName;
-import com.myfiziq.sdk.fragments.FragmentCapture;
 
 /**
  * Creates a route to visit the Capture screen.
@@ -63,22 +61,7 @@ public class StateCapture
     {
         boolean practiseMode = ModelSetting.getSetting(ModelSetting.Setting.FEATURE_PRACTISE_MODE, false);
 
-        return builder
-                .setName(ParameterSetName.MYFIZIQ_SDK_PARENT_ACTIVITY)
-                .addNextSet(new ParameterSet.Builder(FragmentCapture.class)
-                        .setName(ParameterSetName.CAPSIDE)
-                        .setNext(ParameterSetName.CONFIRM_FR)
-                        .addParam(new Parameter(R.id.TAG_ARG_CAPTURE_PRACTISE_MODE, Boolean.toString(practiseMode)))
-                        .build())
-//                .addNextSet(new ParameterSet.Builder(FragmentConfirm.class)
-//                        .setName("CONFIRM_FR")
-//                        .build())
-//                .addNextSet(new ParameterSet.Builder(FragmentRetake.class)
-//                        .setName("RETAKE")
-//                        .setNext("CONFIRM_FR")
-//                        .addParam(new Parameter(com.myfiziq.sdk.R.id.textViewTitle, com.myfiziq.sdk.R.styleable.MyFiziqSdk_MYQ_text, "Retaking Picture...")) //TODO: get string resource
-//                        .build())
-                .build();
+        return builder.build();
 
     }
 }
